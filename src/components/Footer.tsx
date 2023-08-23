59dc391835bee56ef360a550dcafbe2efb29b618
Miks <michaelangelocasal@gmail.com>
2023-08-24 02:35:51 +0800

    adjusted footer style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,20 +3,11 @@ import { Box, Button, Grid, IconButton, Link, Typography } from "@mui/material";
 
 // icons
 import TwitterIcon from "@mui/icons-material/Twitter";
-import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 
 export default function Footer() {
-  /**
-   * why not make a new custom btn?
-   *
-   * - it's too much of a hassle. I'll have to name it, and will cause more time to understand all the custom components in the future.
-   * I prefer making a new variant of the
-   * existing mui components instead of creating new custom components
-   * */
-
   const coolBtnProps = {
     disableRipple: true,
     disableFocusRipple: true,
@@ -29,61 +20,63 @@ export default function Footer() {
         container
         justifyContent="center"
         spacing={3}
-        pt={2}
-        pb={2}
+        pb={10}
         color="white"
       >
         <Grid item xs={9} lg={7}>
           <Grid container spacing={1} color="white">
-            <Grid item xs="auto" md={4}>
+            <Grid item xs={12} md="auto">
               <Button
-                disableRipple
+                {...coolBtnProps}
                 variant="text"
                 component={Link}
                 href="https://twitter.com/mikscasal"
                 startIcon={<TwitterIcon />}
-                sx={{ textTransform: "none", fontSize: "large" }}
+                sx={{ textTransform: "none" }}
               >
                 Follow me on twitter
               </Button>
             </Grid>
-            <Grid item xs="auto">
+
+            <Grid item xs={12} md="auto">
               <Button
-                disableRipple
+                 {...coolBtnProps}
                 component={Link}
                 href="https://www.linkedin.com/in/mikscasal/"
                 variant="text"
                 startIcon={<LinkedInIcon sx={{ color: "#0077B5" }} />}
-                sx={{ textTransform: "none", fontSize: "large" }}
+                sx={{ textTransform: "none"  }}
               >
                 Let's connect on LinkedIn
               </Button>
             </Grid>
-          </Grid>
-        </Grid>
 
-        <Grid item xs={9} lg={7}>
-          <Grid container spacing={1}>
-            <Grid item>
-              <IconButton
+            <Grid item xs={12} md="auto">
+              <Button
+               {...coolBtnProps}
                 component={Link}
                 href="https://github.com/mikskywalker"
                 color="primary"
                 aria-label="github"
+                startIcon={<GitHubIcon />}
+                sx={{ textTransform: "none" }}
               >
-                <GitHubIcon />
-              </IconButton>
+                Github
+              </Button>
             </Grid>
 
-            <Grid item>
-              <IconButton
+            <Grid item  xs={12} md="auto">
+              <Button
+                {...coolBtnProps}
                 component={Link}
-                href="https://www.youtube.com/channel/UCPfm7j1Wm-S7hmUgk49nf8gr"
+                href='https://youtube.com/@MiksCasal'
                 color="primary"
                 aria-label="youtube"
+                startIcon={<YouTubeIcon />}
+                sx={{ textTransform: "none" }}
               >
-                <YouTubeIcon />
-              </IconButton>
+               Youtube 
+              </Button>
             </Grid>
 
           </Grid>
